e1675a74f9bb215d1ece9850700273810e34b377
Baptiste <baptiste@troopers.email>
2019-01-16 15:27:54 +0100

    Initial commit

---
+++ blog/src/Controller/CategoryController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/categories")
+ */
+class CategoryController
+{
+    function getAllCategories() {
+        $categories= [
+            ["name" => "Categorie 1"],
+            ["name" => "Categorie 2"],
+            ["name" => "Categorie 3"],
+        ];
+        $jsonResponse = json_encode($categories);
+        return new Response($jsonResponse);
+    }
+
+    function getCategory($id) {
+        $category= [
+            "id" => $id,
+            "name" => "Catégorie ".$id
+        ];
+
+        $jsonResponse = json_encode($category);
+
+        return new Response($jsonResponse);
+    }
+}
